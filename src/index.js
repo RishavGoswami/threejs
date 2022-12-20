0df76cf4a0b27399a876a05a614ad1efffad28d1
Rishav <36010407+RishavGoswami@users.noreply.github.com>
2022-12-20 18:02:16 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+import { Suspense } from 'react'
+import { Stats } from '@react-three/drei'
+import { Leva } from 'leva'
+import './styles.css'
+import App from './App'
+
+createRoot(document.getElementById('root')).render(
+  <Suspense fallback={null}>
+    <App />
+    <Stats />
+  </Suspense>
+)
